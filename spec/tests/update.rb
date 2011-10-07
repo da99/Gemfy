d45bf1ce45e3892c7fcda8ea52c7c58b708938e1
da99 <i-hate-spam-45671204@mailinator.com>
2011-10-07 03:55:30 -0500

    Faster tests by creating fewer gems.

--- spec/tests/update.rb
+++ spec/tests/update.rb
@@ -9,15 +9,14 @@ describe "Update a gem version" do
   end
 
   it 'bumps minor' do
-    BOX.bin "create joey2"
-    b = BOX.down('joey2')
+    b = BOX.down('joey')
     b.bin "bump_minor"
-    b.read("lib/joey2/version.rb")[/.\d.\d.\d./].should.match %r!.0.1.0.!
+    b.read("lib/joey/version.rb")[/.\d.\d.\d./].should.match %r!.0.1.0.!
   end
   
   it 'adds version.rb to git after bump' do
-    text = %*# On branch master\n#\n# Initial commit\n#\n# Changes to be committed:\n#   (use \"git rm --cached <file>...\" to unstage)\n#\n#\tnew file:   .gitignore\n#\tnew file:   Gemfile\n#\tnew file:   Rakefile\n#\tnew file:   joey2.gemspec\n#\tnew file:   lib/joey2.rb\n#\tnew file:   lib/joey2/version.rb\n#\tnew file:   spec/helper.rb\n#\tnew file:   spec/main.rb\n#*
-    b = BOX.down('joey2')
+    text = %*# On branch master\n#\n# Initial commit\n#\n# Changes to be committed:\n#   (use \"git rm --cached <file>...\" to unstage)\n#\n#\tnew file:   .gitignore\n#\tnew file:   Gemfile\n#\tnew file:   Rakefile\n#\tnew file:   joey.gemspec\n#\tnew file:   lib/joey.rb\n#\tnew file:   lib/joey/version.rb\n#\tnew file:   spec/helper.rb\n#\tnew file:   spec/main.rb\n#*
+    b = BOX.down('joey')
     b.bin( "bump_minor" )
     b.shell('git status').should.be == text
   end
