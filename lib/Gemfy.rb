5c04d2724819be99f9a8d5d1abd5b32470f5c40e
da99 <i-hate-spam-45671204@mailinator.com>
2012-04-27 04:25:36 -0500

    Fixed: 'git push origin vTAG', not 'git push'

--- lib/Gemfy.rb
+++ lib/Gemfy.rb
@@ -297,7 +297,7 @@ class Gemfy
     print "\nPushing gem..."
     shell "gem push #{name}-#{version}.gem"
     shell "rm #{name}-#{version}.gem"
-    shell "git push" if `git remote -v`["origin"]
+    shell "git push origin v#{version}" if `git remote -v`["origin"]
     true
   end
   
